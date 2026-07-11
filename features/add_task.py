6bee95ac1dc4c8c280a3f79def11e0a30430602b
mohamedy72 <mohamedwebdev94@gmail.com>
2026-07-11 17:45:33 +0300

    refactor: remove unncessarly code

--- features/add_task.py
+++ features/add_task.py
@@ -6,12 +6,11 @@ from datetime import datetime
 def add_task(new_task: dict, tasks: list):
     # Getting new id
     new_id = len(tasks) + 1
-    new_tasks = tasks.append({
+    tasks.append({
         "id": new_id,
         "description": new_task,
         "status": "todo",
-        "createdAt": datetime.now(),
-        "updatedAt": datetime.now()
+        "createdAt": str(datetime.now()),
+        "updatedAt": str(datetime.now())
     })
     print(f"Task added successfully (ID: {new_id})")
-    return new_tasks
