67c968e5cf066c0d5b5f776f31748cafd1e5f624
mohamedy72 <mohamedwebdev94@gmail.com>
2026-07-12 15:40:22 +0300

    fix: list all tasks without filters

--- features/list_tasks.py
+++ features/list_tasks.py
@@ -3,17 +3,21 @@ A module that contains funcionality about listing tasks (all, done, not done, in
 """
 
 
-def list_tasks(tasks: list, status: str = "todo"):
+def list_tasks(tasks: list, status: str):
     if not tasks:
         print("No tasks found, add new tasks to view them")
 
-    filtered_tasks = list(filter(lambda item: item["status"] == status, tasks))
+    tasks_to_print = []
+    if status:
+        tasks_to_print = list(filter(lambda item: item["status"] == status, tasks))
+    else:
+        tasks_to_print = tasks
 
-    for task in filtered_tasks:
-        print(f"""
-        Task ID: {task["id"]}
-        Description: {task["description"]}
-        Status: {task["status"]}
-        Task Created at: {task["createdAt"]}
-        Last Update at: {task["updatedAt"] if task["updatedAt"] else "No modification happened"}
-        """)
+    for task in tasks_to_print:
+            print(f"""
+            Task ID: {task["id"]}
+            Description: {task["description"]}
+            Status: {task["status"]}
+            Task Created at: {task["createdAt"]}
+            Last Update at: {task["updatedAt"] if task["updatedAt"] else "No modification happened"}
+            """)
